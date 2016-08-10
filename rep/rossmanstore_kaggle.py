c60ee664fd8a8ada70d474b3c72b08b1358739cc
Anurag Pandey <anuragpandeyiit@gmail.com>
2016-08-10 15:08:04 +0530

    Update rossmanstore_kaggle.py

--- rep/rossmanstore_kaggle.py
+++ rep/rossmanstore_kaggle.py
@@ -1,13 +1,25 @@
+# the rossmand store competition was for predicting the sales by store ID 
+
 import pandas as pd 
 train = pd.read_csv("trainpandas.csv")
 output_file = 'anuragkaggle1994.csv'
 test = pd.read_csv("testpandas.csv")
+# i found some days where sales was zero so it was useless to use it. 
 train = train.loc[train.Sales > 0 ]
 print(train.head())
-median_by_week = train.groupby(['DayOfWeek' , 'Store' , 'Promo'])['Sales'].mean()
+
+
+mean_by_week = train.groupby(['DayOfWeek' , 'Store' , 'Promo'])['Sales'].mean()
+# convert it into DataFrame using reset.index()
+
 median_by_week = median_by_week.reset_index()
-test2 = pd.merge( test , median_by_week , on = ['DayOfWeek' , 'Store' , 'Promo'], how = 'left')
-assert(len(test2) == len(test))
+
+test_02 = pd.merge( test , median_by_week , on = ['DayOfWeek' , 'Store' , 'Promo'], how = 'left')
+
 #print(test2.head())
 test2[['Id' , 'Sales']].to_csv(output_file , index = False)
 #print(test.head())
+
+
+# this type of analysis is very new to me and i am still learning 
+THe pandas tutorial on kaggle was the reason i made this .
